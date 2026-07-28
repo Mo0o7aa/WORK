cb0fb26c4968cf606eb9ae10a18128d69fb2b734
asameh <ahmmedsamehh@gmail.com>
2026-07-28 04:56:06 -0700

    fixed mission feedback bug and enhanced add mission window

--- MissionFeedbackWindow.xaml.cs
+++ MissionFeedbackWindow.xaml.cs
@@ -46,76 +46,90 @@ namespace ntra_missions
 
         public void InitializeSitesGrid()
         {
+            sitesStackPanel.Children.Clear();
+
             for (int i = 0; i < mission.GetSites().Count; i++)
             {
+                int siteSerial = mission.GetSites()[i].site_serial;
 
-                BrushConverter brushConverter = new BrushConverter();
+                // the sectors for this site come from the complaint (they carry
+                // the sector_serial the DB update filters on)
+                BASIC_STRUCTS.SITE_STRUCT complaintSite =
+                    mission.complaint.GetComplaintsSites().Find(x1 => x1.site_serial == siteSerial);
 
-                Border border = new Border() { BorderThickness = new Thickness(5) };
-                border.BorderBrush = (Brush)brushConverter.ConvertFrom("#000080");
+                Border border = new Border()
+                {
+                    Style = (Style)FindResource("RowCardBorder"),
+                    Margin = new Thickness(0, 0, 0, 12)
+                };
 
                 Grid currentSiteGrid = new Grid();
-                currentSiteGrid.ColumnDefinitions.Add(new ColumnDefinition());
-                currentSiteGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                currentSiteGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(240) });
+                currentSiteGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto });
                 currentSiteGrid.ColumnDefinitions.Add(new ColumnDefinition());
 
                 //////////////SITE NUMBER//////////////////
-                WrapPanel currentSiteNumberWrapPanel = new WrapPanel();
-                currentSiteNumberWrapPanel.VerticalAlignment = VerticalAlignment.Center;
-                currentSiteNumberWrapPanel.HorizontalAlignment = HorizontalAlignment.Center;
+                StackPanel siteInfoPanel = new StackPanel { VerticalAlignment = VerticalAlignment.Center };
 
-                Label currentSiteNumberLabel = new Label();
-                currentSiteNumberLabel.Style = (Style)FindResource("mediumLabelStyleBlack");
-                currentSiteNumberLabel.Content = "Site Number: ";
+                siteInfoPanel.Children.Add(new TextBlock
+                {
+                    Text = "Site Number",
+                    Style = (Style)FindResource("FieldLabelText")
+                });
+                siteInfoPanel.Children.Add(new TextBlock
+                {
+                    Text = mission.GetSites()[i].site_name,
+                    Style = (Style)FindResource("CardTitleText"),
+                    Margin = new Thickness(0, 2, 0, 0)
+                });
 
-                Label currentSiteNumberLabelValue = new Label();
-                currentSiteNumberLabelValue.Style = (Style)FindResource("labelStyle");
-                currentSiteNumberLabelValue.Content = mission.GetSites()[i].site_name;
-
-                currentSiteNumberWrapPanel.Children.Add(currentSiteNumberLabel);
-                currentSiteNumberWrapPanel.Children.Add(currentSiteNumberLabelValue);
-
-                currentSiteGrid.Children.Add(currentSiteNumberWrapPanel);
-                Grid.SetColumn(currentSiteNumberWrapPanel, 0);
-
+                currentSiteGrid.Children.Add(siteInfoPanel);
+                Grid.SetColumn(siteInfoPanel, 0);
 
                 //////////////SECTORS//////////////////
-
-                Label currentSectorLabel = new Label();
-                currentSectorLabel.Style = (Style)FindResource("wideLabelStyleBlack");
-                currentSectorLabel.Content = "Select enhanced sectors: ";
-                currentSectorLabel.VerticalAlignment = VerticalAlignment.Center;
+                TextBlock currentSectorLabel = new TextBlock
+                {
+                    Text = "Enhanced sectors:",
+                    Style = (Style)FindResource("FieldLabelText"),
+                    VerticalAlignment = VerticalAlignment.Center,
+                    Margin = new Thickness(0, 0, 16, 0)
+                };
 
                 currentSiteGrid.Children.Add(currentSectorLabel);
                 Grid.SetColumn(currentSectorLabel, 1);
 
+                WrapPanel sectorsInnerPanel = new WrapPanel { VerticalAlignment = VerticalAlignment.Center };
 
-                StackPanel sectorsInnerStackPanel = new StackPanel();
-                for (int j = 0; j < mission.complaint.GetComplaintsSites().Find(x1 => x1.site_serial == mission.GetSites()[i].site_serial).sectors.Count; j++)
+                if (complaintSite.sectors != null)
                 {
-                    CheckBox currentSectorCheckBox = new CheckBox();
-                    currentSectorCheckBox.Style = (Style)FindResource("checkBoxStyle");
-                    currentSectorCheckBox.Margin = new Thickness(12);
-                    currentSectorCheckBox.HorizontalAlignment = HorizontalAlignment.Center;
-                    currentSectorCheckBox.Cursor = Cursors.Hand;
-                    currentSectorCheckBox.Content = "Sector " + mission.complaint.GetComplaintsSites().Find(x1 => x1.site_serial == mission.GetSites()[i].site_serial).sectors[j].sector_number;
-                    currentSectorCheckBox.Tag = mission.GetSites()[i].site_serial + "," + mission.complaint.GetComplaintsSites().Find(x1 => x1.site_serial == mission.GetSites()[i].site_serial).sectors[j].sector_number;
-                    currentSectorCheckBox.Checked += OnCheckCurrentSectorCheckBox;
-                    currentSectorCheckBox.Unchecked += OnUnCheckCurrentSectorCheckBox;
+                    for (int j = 0; j < complaintSite.sectors.Count; j++)
+                    {
+                        BASIC_STRUCTS.SECTOR_STRUCT sector = complaintSite.sectors[j];
 
-                    if(mission.complaint.GetComplaintsSites().Find(x1 => x1.site_serial == mission.GetSites()[i].site_serial).sectors[j].sector_status_id == BASIC_STRUCTS.CLEARED_SECTOR_STATUS)
-                        currentSectorCheckBox.IsChecked = true;
+                        CheckBox currentSectorCheckBox = new CheckBox();
+                        currentSectorCheckBox.Style = (Style)FindResource("checkBoxStyle");
+                        currentSectorCheckBox.Width = double.NaN;
+                        currentSectorCheckBox.Margin = new Thickness(0, 6, 20, 6);
+                        currentSectorCheckBox.Cursor = Cursors.Hand;
+                        currentSectorCheckBox.Content = "Sector " + sector.sector_number;
+                        // Tag carries the sector_serial (what the DB update filters on)
+                        currentSectorCheckBox.Tag = siteSerial + "," + sector.sector_serial;
+                        currentSectorCheckBox.Checked += OnCheckCurrentSectorCheckBox;
+                        currentSectorCheckBox.Unchecked += OnUnCheckCurrentSectorCheckBox;
 
-                    sectorsInnerStackPanel.Children.Add(currentSectorCheckBox);
+                        if (sector.sector_status_id == BASIC_STRUCTS.CLEARED_SECTOR_STATUS)
+                            currentSectorCheckBox.IsChecked = true;
+
+                        sectorsInnerPanel.Children.Add(currentSectorCheckBox);
+                    }
                 }
 
-                currentSiteGrid.Children.Add(sectorsInnerStackPanel);
-                Grid.SetColumn(sectorsInnerStackPanel, 2);
+                currentSiteGrid.Children.Add(sectorsInnerPanel);
+                Grid.SetColumn(sectorsInnerPanel, 2);
 
                 border.Child = currentSiteGrid;
 
                 sitesStackPanel.Children.Add(border);
-
             }
         }
 
@@ -176,6 +190,8 @@ namespace ntra_missions
             // complaint whose sites are now fully cleared
             if (!mission.complaint.UpdateMissionStatuses())
                 return;
+
+            MessageBox.Show("Feedback saved.", "Done", MessageBoxButton.OK, MessageBoxImage.Information);
 
             this.Close();
         }
